80bb5638d622461fe42eda259cea6b02674f8a49
Nikitas <93310518+NikitasDiscord@users.noreply.github.com>
2023-12-24 18:27:57 +0700

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -80,6 +80,7 @@ BOARD_SUPER_PARTITION_GROUPS := itel_dynamic_partitions
 BOARD_ITEL_DYNAMIC_PARTITIONS_PARTITION_LIST := system system_ext vendor product
 BOARD_ITEL_DYNAMIC_PARTITIONS_SIZE := 9122611200 # TODO: Fix hardcoded value
 TW_DEVICE_VERSION := nikitas @4pda
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery/root/system/etc/recovery.fstab
 # Platform
 TARGET_BOARD_PLATFORM := ums9230
 
